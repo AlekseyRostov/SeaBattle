f7372a19dae5c9ebbf5088511d7aba87e5624a24
ARostov <aarostov@alfabank.ru>
2021-03-31 00:29:58 +0300

    Added test

---
+++ SeaBattle.Domain.Test/BattleTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SeaBattle.Domain.Exceptions;
+
+namespace SeaBattle.Domain.Test
+{
+    public class BattleTest
+    {
+        [Test]
+        public void Battle_CreateShipsBeforeBattle_ThrowException()
+        {
+            var battle = new Battle();
+            var shipsCoordinates = new List<ShipCoordinates>();
+
+            Assert.Throws<InvalidBusinessLogicException>(() => battle.CreateShips(shipsCoordinates));
+        }
+        
+        [Test]
+        public void Battle_CreateShipsRepeatedly_ThrowException()
+        {
+            var battle = new Battle();
+            battle.CreateBattle(10);
+            var shipsCoordinates = new List<ShipCoordinates>();
+            shipsCoordinates.Add(new ShipCoordinates(new[] { new Coordinates("1A"), new Coordinates("2B") }));
+
+            battle.CreateShips(shipsCoordinates);
+
+            Assert.Throws<InvalidBusinessLogicException>(() => battle.CreateShips(shipsCoordinates));
+        }
+    }
+}
